30c7d61d0e5954b4be5ebe79514be6568a67edd1
YOUR_NAME <YOUR_EMAIL>
2023-12-29 21:54:25 +0300

    Set the chessboard

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setLayout(new GridBagLayout());
+        frame.setMinimumSize(new Dimension(1000, 1000));
+        frame.setLocationRelativeTo(null);
+
+        Board board = new Board();
+        frame.add(board);
+
+        frame.setVisible(true);
+    }
+}
